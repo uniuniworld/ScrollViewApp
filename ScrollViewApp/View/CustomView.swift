203dd769e45e9eaadd1dd9f5c332ba9993453447
kirifu_mini <uniuniunu2525@gmail.com>
2020-10-21 11:02:07 +0900

    各Labelを追加 修正

---
+++ ScrollViewApp/View/CustomView.swift
@@ -0,0 +1,41 @@
+//
+//  CustomView.swift
+//  ScrollViewApp
+//
+//  Created by Takahiro Kirifu on 2020/10/21.
+//  Copyright © 2020 Takahiro Kirifu. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable class CustomView: UIView {
+    
+    @IBOutlet weak var itemLabel: UILabel!
+    @IBOutlet weak var contentLabel: UILabel!
+    
+
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        loadNib()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)!
+        loadNib()
+    }
+    
+//    func loadNib() {
+//        let view = Bundle.main.loadNibNamed("CustomView", owner: self, options: nil)?.first as! UIView
+//        view.frame = self.bounds
+//        self.addSubview(view)
+//
+//    }
+    func loadNib() {
+        if let view = Bundle(for: type(of: self)).loadNibNamed(String(describing: type(of: self)), owner: self, options: nil)?.first as? UIView {
+            view.frame = self.bounds
+            self.addSubview(view)
+        }
+        
+    }
+}
--- ScrollViewApp/ViewController.swift
+++ ScrollViewApp/ViewController.swift
@@ -10,9 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var houseCallView: CustomView!
+    @IBOutlet weak var locationView: CustomView!
+    @IBOutlet weak var requestView: CustomView!
+    @IBOutlet weak var detailView: CustomView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setContent(view: houseCallView, itemName: "往診日時", content: "6/23 15:05")
+        setContent(view: locationView, itemName: "往診場所", content: "老人ホーム\r\n460-3333\r\n名古屋市中区なんとか二丁目666-666なんとかホーム")
+        setContent(view: requestView, itemName: "申請", content: "訪問介護ステーション ナーシングみなみみなみ")
+        setContent(view: detailView, itemName: "申請内容", content: "fadfjmelkf;jeklfmeklwmflekjfp'e\r\njmflkjekl;fjew'lkjf'ewjkl'rjelkje'kjfの書の世に出づるにいたりた\r\nるは、函館にある秦慶治氏、及び信濃にある神津猛氏のたまものなり。労作終る\r\nの日にあたりて、このものがたりを二人の恩人のまへにさゝぐ。'ewkj'kfejkfjekl'jfw'jfklejej'fejwl'fkje私はそのｙ人を常に先生\r\nとよんでいた。")
+        
+        
         // Do any additional setup after loading the view.
+    }
+    
+    private func setContent(view: CustomView, itemName: String, content: String) {
+        
+        view.itemLabel.text = itemName
+        view.contentLabel.text = content
+        
+        view.contentLabel.numberOfLines = 0
+        view.contentLabel.lineBreakMode = .byCharWrapping
+        view.contentLabel.sizeToFit()
+        
+        view.frame.size.height = view.contentLabel.frame.size.height + 20
+        
     }
 
 
